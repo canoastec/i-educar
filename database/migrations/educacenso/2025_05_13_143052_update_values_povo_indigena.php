d94eb7659ab93ed81adcd5a722af80ea3f946f0e
Edinei Valdameri <edineivaldameri@gmail.com>
2025-05-13 14:39:49 -0300

    Adiciona tabela e model para povos indígenas

---
+++ database/migrations/educacenso/2025_05_13_143052_update_values_povo_indigena.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\EducacensoIndigenousPeople;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $file = file(database_path('csv/censo/2025/povos_indigenas.csv'));
+
+        foreach ($file as $line) {
+            $data = str_getcsv(
+                string: $line,
+                separator: ';'
+            );
+
+            EducacensoIndigenousPeople::query()
+                ->create([
+                    'id' => $data[0],
+                    'name' => $data[1],
+                ]);
+        }
+    }
+};
