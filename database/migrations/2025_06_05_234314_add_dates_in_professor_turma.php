9322b80b4d9ee501fc5fa4ea75f9670eb460274f
Robert Ferraz <robert@portabilis.com.br>
2025-06-10 15:37:54 -0300

    Adiciona scheme da tabela da migration

--- database/migrations/2025_06_05_234314_add_dates_in_professor_turma.php
+++ database/migrations/2025_06_05_234314_add_dates_in_professor_turma.php
@@ -10,7 +10,7 @@ return new class extends Migration {
 
     public function up(): void
     {
-        Schema::table('professor_turma', function (Blueprint $table) {
+        Schema::table('modules.professor_turma', function (Blueprint $table) {
             $table->date('data_inicial')->nullable();
             $table->date('data_fim')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration {
     {
         $this->createView('public.educacenso_record50', '2024-05-23');
 
-        Schema::table('professor_turma', function (Blueprint $table) {
+        Schema::table('modules.professor_turma', function (Blueprint $table) {
             $table->dropColumn('data_inicial');
             $table->dropColumn('data_fim');
         });
