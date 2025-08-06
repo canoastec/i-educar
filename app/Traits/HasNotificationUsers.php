33b9c3afb21afe8547136783e5c0bc55b465c25d
Robert Ferraz <robert@portabilis.com.br>
2025-08-06 10:45:17 -0300

    Corrige query dos usuários notificados

--- app/Traits/HasNotificationUsers.php
+++ app/Traits/HasNotificationUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use App\Models\LegacyUserType;
 use Illuminate\Support\Facades\DB;
 
 trait HasNotificationUsers
@@ -13,21 +14,32 @@ trait HasNotificationUsers
      * @param int $school
      * @return array
      */
-    public function getUsers($process, $school)
+    public function getUsers(int $process, int $school)
     {
         return DB::table('pmieducar.usuario as u')
             ->select('u.cod_usuario')
-            ->join('pmieducar.menu_tipo_usuario as mtu', fn ($j) => $j->on('mtu.ref_cod_tipo_usuario', 'u.ref_cod_tipo_usuario')->where('mtu.visualiza', 1))
+            ->distinct()
             ->join('pmieducar.tipo_usuario as tu', 'tu.cod_tipo_usuario', 'u.ref_cod_tipo_usuario')
-            ->join('public.menus as m', 'm.id', 'mtu.menu_id')
             ->leftJoin('pmieducar.escola_usuario as eu', 'eu.ref_cod_usuario', 'u.cod_usuario')
-            ->where('m.process', $process)
+            // menus somente para quem não é Admin
+            ->leftJoin('pmieducar.menu_tipo_usuario as mtu', function ($j) {
+                $j->on('mtu.ref_cod_tipo_usuario', 'u.ref_cod_tipo_usuario');
+                $j->where('mtu.visualiza', 1);
+            })
+            ->leftJoin('public.menus as m', 'm.id', 'mtu.menu_id')
             ->where('u.ativo', 1)
+            // condição de escola ou nível institucional
             ->where(function ($q) use ($school) {
                 $q->where('eu.ref_cod_escola', $school);
-                $q->orWhere('tu.nivel', '<=', \App_Model_NivelTipoUsuario::INSTITUCIONAL);
+                $q->orWhere('tu.nivel', '<=', LegacyUserType::LEVEL_INSTITUTIONAL);
+            })
+            // condição de menu: ou é Admin, ou tem menu válido no processo
+            ->where(function ($q) use ($process) {
+                $q->where('tu.nivel', LegacyUserType::LEVEL_ADMIN);
+                $q->orWhere('m.process', $process);
             })
             ->get()
             ->toArray();
     }
+
 }
