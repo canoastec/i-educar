f111235e4cf7171b806c8f5bc9deffaec0896e19
lucas.cunha <lucas.cunha@canoastec.rs.gov.br>
2026-09-09 15:26:15 -0300

    #9865 - Liberação e Fechamento Manual de Prova para as Minhas Turmas

--- public/vendor/legacy/DynamicInput/Assets/Javascripts/DisciplinaProva.js
+++ public/vendor/legacy/DynamicInput/Assets/Javascripts/DisciplinaProva.js
@@ -6,36 +6,53 @@
     var $disciplinaField = getElementFor('disciplina_prova');
     var $provaField = getElementFor('prova');
 
+    $disciplinaField.attr('data-no-autocomplete', 'true');
+
+    var refreshChosen = function($field) {
+      if (!$field || !$field.length) {
+        return;
+      }
+
+      $field.trigger('chosen:updated');
+    };
+
     var handleGetDisciplinas = function(resources) {
       var selectOptions = jsonResourcesToSelectOptions(resources['options']);
       updateSelect($disciplinaField, selectOptions, "Selecione uma disciplina");
-      $disciplinaField.trigger('chosen:updated');
+      $disciplinaField.val('');
+      refreshChosen($disciplinaField);
+      $disciplinaField.change();
     };
 
     var updateDisciplinas = function(){
       resetSelect($disciplinaField);
       resetSelect($provaField);
-      $disciplinaField.trigger('chosen:updated');
-      $provaField.trigger('chosen:updated');
 
-      if ($anoField.val() && $serieField.val()) {
-        $disciplinaField.children().first().html('Aguarde carregando...');
+      $disciplinaField.val('');
+      $provaField.val('');
 
-        var url = getResourceUrlBuilder.buildUrl('/module/DynamicInput/prova', 'disciplinas', {
-          ano: $anoField.val(),
-          serie_prova: $serieField.val()
-        });
+      $disciplinaField.children().first().html('Selecione uma disciplina');
+      refreshChosen($disciplinaField);
+      refreshChosen($provaField);
 
-        var options = {
-          url: url,
-          dataType: 'json',
-          success: handleGetDisciplinas
-        };
-
-        getResources(options);
+      if (!($anoField.val() && $serieField.val())) {
+        $disciplinaField.change();
+        return;
       }
 
-      $disciplinaField.change();
+      $disciplinaField.children().first().html('Aguarde carregando...');
+      refreshChosen($disciplinaField);
+
+      var url = getResourceUrlBuilder.buildUrl('/module/DynamicInput/prova', 'disciplinas', {
+        ano: $anoField.val(),
+        serie_prova: $serieField.val()
+      });
+
+      getResources({
+        url: url,
+        dataType: 'json',
+        success: handleGetDisciplinas
+      });
     };
 
     $anoField.change(updateDisciplinas);
@@ -43,7 +60,3 @@
 
   });
 })(jQuery);
-
-
-
-
--- public/vendor/legacy/DynamicInput/Assets/Javascripts/Habilidade.js
+++ public/vendor/legacy/DynamicInput/Assets/Javascripts/Habilidade.js
@@ -3,44 +3,68 @@
 
     var $codigoField = getElementFor('codigo_habilidade');
     var $habilidadeField = getElementFor('habilidade');
+    var $eixoField = getElementFor('eixo_conhecimento');
 
-    var handleGetHabilidades = function(resources) {
-      var selectOptions = jsonResourcesToSelectOptions(resources['options']);
-      updateSelect($habilidadeField, selectOptions, "Todas as habilidades");
+    $habilidadeField.attr('data-no-autocomplete', 'true');
+
+    var refreshChosen = function($field) {
+      if (!$field || !$field.length) {
+        return;
+      }
+
+      if ($field.hasClass('chzn-done')) {
+        $field.trigger('liszt:updated');
+      }
+
+      $field.trigger('chosen:updated');
     };
 
-    var loadHabilidades = function(){
-      resetSelect($habilidadeField);
-      $habilidadeField.children().first().html('Aguarde carregando...');
-
-      var url = getResourceUrlBuilder.buildUrl('/module/DynamicInput/habilidade', 'habilidades', {});
-
-      var options = {
-        url: url,
-        dataType: 'json',
-        success: handleGetHabilidades
-      };
-
-      getResources(options);
-
-      $habilidadeField.change();
-    };
-
-    // Espelha a seleção da habilidade no campo Código.
-    $habilidadeField.change(function(){
+    var syncCodigoFromHabilidade = function(){
       if (!$codigoField.length) {
         return;
       }
 
-      var value = $habilidadeField.val();
+      var value = $habilidadeField.val() || '';
 
       if ($codigoField.val() !== value) {
         $codigoField.val(value);
-        if ($codigoField.hasClass('chzn-done')) {
-          $codigoField.trigger('liszt:updated');
-        }
+        refreshChosen($codigoField);
       }
-    });
+    };
+
+    var handleGetHabilidades = function(resources) {
+      var selectOptions = jsonResourcesToSelectOptions(resources['options']);
+      updateSelect($habilidadeField, selectOptions, "Todas as habilidades");
+      $habilidadeField.val('');
+      refreshChosen($habilidadeField);
+      syncCodigoFromHabilidade();
+    };
+
+    var loadHabilidades = function(){
+      if (!$habilidadeField.length) {
+        return;
+      }
+
+      resetSelect($habilidadeField);
+      $habilidadeField.children().first().html('Aguarde carregando...');
+      refreshChosen($habilidadeField);
+
+      var url = getResourceUrlBuilder.buildUrl('/module/DynamicInput/habilidade', 'habilidades', {
+        eixo_conhecimento: $eixoField.length ? ($eixoField.val() || '') : ''
+      });
+
+      getResources({
+        url: url,
+        dataType: 'json',
+        success: handleGetHabilidades
+      });
+    };
+
+    $habilidadeField.change(syncCodigoFromHabilidade);
+
+    if ($eixoField.length) {
+      $eixoField.change(loadHabilidades);
+    }
 
     loadHabilidades();
 
